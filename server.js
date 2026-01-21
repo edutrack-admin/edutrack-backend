f517b1c5d087a2efe0b2b110c83abc1033ae6d3a
delosreyeskaizerluis-cell <delosreyes.kaizerluis@gmail.com>
2026-01-21 14:29:19 +0800

    added email import to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { transporter } from './utils/email.js';
 
 // Load environment variables
 dotenv.config();
